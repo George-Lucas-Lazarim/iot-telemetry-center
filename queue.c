518c19b72f27e731a56c72a670c37a1ec04d3ec3
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-04-21 11:58:41 -0300

    createQueueFunction

--- queue.c
+++ queue.c
@@ -1,1 +1,9 @@
 #include "queue.h"
+
+struct queue* createQueue (struct queue* queue) {
+    queue = (struct queue*) malloc (sizeof(struct queue));
+    if (queue == NULL) return queue;
+
+    queue->front = queue->rear = NULL;
+    return queue;
+}
