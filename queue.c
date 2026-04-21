cf4084a4c6b4f393e68d64724816e87016b4cf2c
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-04-21 12:09:33 -0300

    enqueueFunction

--- queue.c
+++ queue.c
@@ -1,9 +1,29 @@
 #include "queue.h"
 
-struct queue* createQueue (struct queue* queue) {
-    queue = (struct queue*) malloc (sizeof(struct queue));
+struct queue* createQueue() {
+    struct queue* queue = (struct queue*) malloc (sizeof(struct queue));
     if (queue == NULL) return queue;
 
     queue->front = queue->rear = NULL;
     return queue;
+}
+
+void enqueue (struct queue* queue, float data, int id) {
+    struct queueNode* newNode = (struct queueNode*) malloc (sizeof(struct queueNode));
+
+    if (newNode == NULL) {
+        printf("Error! Memory allocation failure");
+        return;
+    }
+
+    newNode->data = data;
+    newNode->id = id;
+    newNode->next = NULL;
+
+    if (queue == NULL) {
+        queue->front = queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
 }
--- queue.h
+++ queue.h
@@ -1,4 +1,21 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queueNode {
+    float data;
+    int id;
+    struct queueNode* next;
+};
+
+struct queue {
+    struct queueNode* front;
+    struct queueNode* rear;
+};
+
+struct queue* createQueue();
+void enqueue (struct queue* queue, float data, int id);
+
 #endif
