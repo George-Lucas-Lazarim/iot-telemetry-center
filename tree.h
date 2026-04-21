984e9f02ef8310ce85461c9ebdcd151fb3dc5faf
George Lucas Lazarim <georgelucas1402@gmail.com>
2026-04-21 19:34:44 -0300

    debugmergeconflict

--- tree.h
+++ tree.h
@@ -1,4 +1,17 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash.h"
+
+struct treeNode {
+    float data;
+    int id;
+    struct treeNode* right;
+    struct treeNode* left;
+};
+
+struct treeNode* addData (struct treeNode* root, struct node* node);
+
 #endif
